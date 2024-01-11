96671b2316552bad9b41d232d3d81ad28c2f7eec
zultaifhassan <zultaifhassan110@gmail.com>
2024-01-11 15:44:17 +0500

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='Animal-Navbar'>
+        <div className="animal-logo">
+            <h2>FarmGhar</h2>
+        </div>
+        <div className="animal-centerlinks">
+            <ul>
+                
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
